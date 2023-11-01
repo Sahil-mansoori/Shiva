08726da304457b6c65e67d9d3880f46305c4dce1
Sahil-mansoori <sahil997195@gmail.com>
2023-11-01 11:26:41 +0530

    uplodae new webiste

---
+++ app.js
@@ -0,0 +1,4 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'particlesjs-config (1).json', function() {
+    console.log('callback - particles.js config loaded');
+  });
